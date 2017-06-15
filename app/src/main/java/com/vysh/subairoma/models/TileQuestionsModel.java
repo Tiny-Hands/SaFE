0f92bea334bb8ce61c2e90891c963350b274d50e
Vysh1 <vishal.shrestha9794@gmail.com>
2017-06-15 20:39:06 +0545

    Tile and question models added

---
+++ app/src/main/java/com/vysh/subairoma/models/TileQuestionsModel.java
@@ -0,0 +1,74 @@
+package com.vysh.subairoma.models;
+
+/**
+ * Created by Vishal on 6/15/2017.
+ */
+
+public class TileQuestionsModel {
+    String condition, title, description, question;
+    int tileId, questionId, questionNo, responseType;
+
+    public int getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(int questionId) {
+        this.questionId = questionId;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public int getTileId() {
+        return tileId;
+    }
+
+    public void setTileId(int tileId) {
+        this.tileId = tileId;
+    }
+
+    public int getQuestionNo() {
+        return questionNo;
+    }
+
+    public void setQuestionNo(int questionNo) {
+        this.questionNo = questionNo;
+    }
+
+    public int getResponseType() {
+        return responseType;
+    }
+
+    public void setResponseType(int responseType) {
+        this.responseType = responseType;
+    }
+}
---
+++ app/src/main/java/com/vysh/subairoma/models/TilesModel.java
@@ -0,0 +1,42 @@
+package com.vysh.subairoma.models;
+
+/**
+ * Created by Vishal on 6/15/2017.
+ */
+
+public class TilesModel {
+    String type, title, description;
+    int tileId;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getTileId() {
+        return tileId;
+    }
+
+    public void setTileId(int tileId) {
+        this.tileId = tileId;
+    }
+}
