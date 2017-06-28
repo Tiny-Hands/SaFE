724fa3f4e5ed7a7c550e5e8569293358a518f914
Vysh1 <vishal.shrestha9794@gmail.com>
2017-06-28 23:29:29 +0545

    Migrant list model

--- app/src/main/java/com/vysh/subairoma/adapters/MigrantListAdapter.java
+++ app/src/main/java/com/vysh/subairoma/adapters/MigrantListAdapter.java
@@ -5,15 +5,29 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.vysh.subairoma.ActivityTileHome;
 import com.vysh.subairoma.R;
+import com.vysh.subairoma.models.MigrantModel;
+
+import java.util.ArrayList;
 
 /**
  * Created by Vishal on 6/16/2017.
  */
 
 public class MigrantListAdapter extends RecyclerView.Adapter<MigrantListAdapter.MigrantHolder> {
+    public ArrayList<MigrantModel> getMigrants() {
+        return migrants;
+    }
+
+    public void setMigrants(ArrayList<MigrantModel> migrants) {
+        this.migrants = migrants;
+    }
+
+    ArrayList<MigrantModel> migrants;
+
     @Override
     public MigrantHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         return new MigrantHolder(LayoutInflater.from(parent.getContext()).
@@ -22,7 +36,9 @@ public class MigrantListAdapter extends RecyclerView.Adapter<MigrantListAdapter.
 
     @Override
     public void onBindViewHolder(MigrantHolder holder, int position) {
-
+        MigrantModel migrantModel = migrants.get(position);
+        holder.textViewName.setText(migrantModel.getMigrantName());
+        holder.sex.setText(migrantModel.getMigrantSex() + ", " + migrantModel.getMigrantAge());
     }
 
     @Override
@@ -31,9 +47,12 @@ public class MigrantListAdapter extends RecyclerView.Adapter<MigrantListAdapter.
     }
 
     public class MigrantHolder extends RecyclerView.ViewHolder {
+        public TextView textViewName, sex;
 
         public MigrantHolder(final View itemView) {
             super(itemView);
+            textViewName = (TextView) itemView.findViewById(R.id.tvMigrantName);
+            sex = (TextView) itemView.findViewById(R.id.tvMigrantAgeSex);
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
