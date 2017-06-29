ac84b069760a3586cdd0b2c9a2863dd4491b249d
Vysh1 <vishal.shrestha9794@gmail.com>
2017-06-29 07:35:29 +0545

    get migrants method added

--- app/src/main/java/com/vysh/subairoma/ActivityMigrantList.java
+++ app/src/main/java/com/vysh/subairoma/ActivityMigrantList.java
@@ -1,16 +1,32 @@
 package com.vysh.subairoma;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.vysh.subairoma.adapters.MigrantListAdapter;
+import com.vysh.subairoma.volley.VolleyController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -20,11 +36,14 @@ import butterknife.ButterKnife;
  */
 
 public class ActivityMigrantList extends AppCompatActivity {
+    private final String API = "/subairoma/getmigrants.php";
 
     @BindView(R.id.rvMigrants)
     RecyclerView recyclerView;
     @BindView(R.id.btnAddMigrant)
     Button btnAddMigrant;
+    @BindView(R.id.rlRoot)
+    RelativeLayout rootLayout;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -33,6 +52,7 @@ public class ActivityMigrantList extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         ButterKnife.bind(this);
 
+        getMigrants();
         btnAddMigrant.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,8 +63,56 @@ public class ActivityMigrantList extends AppCompatActivity {
         setUpRecyclerView();
     }
 
+    private void getMigrants() {
+        String api = ApplicationClass.getInstance().getAPIROOT() + API;
+        final ProgressDialog progressDialog = new ProgressDialog(ActivityMigrantList.this);
+        progressDialog.setTitle("Please wait");
+        progressDialog.setMessage("Getting migrants...");
+        progressDialog.show();
+        StringRequest saveRequest = new StringRequest(Request.Method.POST, api, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                progressDialog.dismiss();
+                Log.d("mylog", "response : " + response);
+                parseResponse(response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                progressDialog.dismiss();
+                String err = error.toString();
+                Log.d("mylog", "error : " + err);
+                if (!err.isEmpty() && err.contains("TimeoutError"))
+                    showSnackbar("Failed to connect to server :(");
+                else
+                    showSnackbar(error.toString());
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                HashMap<String, String> params = new HashMap<>();
+                params.put("user_id", ApplicationClass.getInstance().getUserId() + "");
+                return params;
+            }
+        };
+        VolleyController.getInstance(getApplicationContext()).addToRequestQueue(saveRequest);
+    }
+
+    private void parseResponse(String response) {
+    }
+
     private void setUpRecyclerView() {
         recyclerView.setLayoutManager(new LinearLayoutManager(ActivityMigrantList.this));
         recyclerView.setAdapter(new MigrantListAdapter());
+    }
+
+    private void showSnackbar(String msg) {
+        Snackbar snack = Snackbar.make(rootLayout, msg, Snackbar.LENGTH_LONG);
+        View view = snack.getView();
+        TextView tv = (TextView) view.findViewById(android.support.design.R.id.snackbar_text);
+        tv.setTextColor(Color.WHITE);
+        if (Build.VERSION.SDK_INT >= 17)
+            tv.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+        snack.show();
     }
 }
