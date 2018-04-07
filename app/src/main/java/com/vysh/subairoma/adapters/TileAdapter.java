06c6772461c45cfa4a879f4954962363e9d228c8
vysh <vishal.shrestha9794@gmail.com>
2018-04-07 11:59:13 +0545

    important contacts tile always visible

--- app/src/main/java/com/vysh/subairoma/adapters/TileAdapter.java
+++ app/src/main/java/com/vysh/subairoma/adapters/TileAdapter.java
@@ -34,6 +34,7 @@ public class TileAdapter extends RecyclerView.Adapter<TileAdapter.TileViewHolder
     Context context;
     int impContactsPlace;
     String cid;
+    int offset = 0;
 
     public TileAdapter(ArrayList list, int impContactsPlace, int[] tiles, Context cxt, String countryId) {
         tileList = list;
@@ -53,27 +54,28 @@ public class TileAdapter extends RecyclerView.Adapter<TileAdapter.TileViewHolder
     @Override
     public void onBindViewHolder(TileViewHolder holder, final int position) {
         //Show Important contacts tile at the bottom
-        if ((position == impContactsPlace && ActivityTileHome.finalSection) || (position == tileList.size() && ActivityTileHome.showIndia)) {
+        if ((position == impContactsPlace) || (position == tileList.size() && ActivityTileHome.showIndia)) {
             holder.tvTile.setText(context.getResources().getString(R.string.important_contacts));
             holder.ivTile.setImageResource(R.drawable.ic_phonebook);
             holder.tvPercent.setVisibility(View.GONE);
-        } else if (position < tileList.size()) {
-            holder.tvTile.setText(tileList.get(position).getTitle());
+            offset = 1;
+        } else if (position <= tileList.size()) {
+            holder.tvTile.setText(tileList.get(position - offset).getTitle());
             if (ActivityTileHome.finalSection) {
-                if (tileList.get(position).getType().equalsIgnoreCase("GAS")) {
+                if (tileList.get(position - offset).getType().equalsIgnoreCase("GAS")) {
                     holder.viewDisabled.setVisibility(View.GONE);
                 } else {
                     holder.viewDisabled.setVisibility(View.VISIBLE);
                 }
             } else {
-                if (tileList.get(position).getType().equals("FEP")) {
+                if (tileList.get(position - offset).getType().equals("FEP")) {
                     holder.viewDisabled.setVisibility(View.GONE);
                 } else
                     holder.viewDisabled.setVisibility(View.VISIBLE);
             }
             int errorCount = sqlDatabaseHelper.getTileErrorCount(ApplicationClass.getInstance().getMigrantId(),
-                    tileList.get(position).getTileId());
-            Log.d("mylog", "Tile Errors: " + errorCount + " For tile ID: " + tileList.get(position).getTileId());
+                    tileList.get(position - offset).getTileId());
+            Log.d("mylog", "Tile Errors: " + errorCount + " For tile ID: " + tileList.get(position - offset).getTileId());
             if (errorCount > 0) {
                 for (int i = 0; i < errorCount; i++) {
                     ImageView imgView = new ImageView(holder.llErrorLayout.getContext());
@@ -85,8 +87,8 @@ public class TileAdapter extends RecyclerView.Adapter<TileAdapter.TileViewHolder
                 }
             }
             DecimalFormat decimalFormat = new DecimalFormat("##");
-            holder.tvPercent.setText(decimalFormat.format(tileList.get(position).getPercentComplete()) + "%");
-            setTileIcons(holder.ivTile, tileList.get(position).getTileId());
+            holder.tvPercent.setText(decimalFormat.format(tileList.get(position - offset).getPercentComplete()) + "%");
+            setTileIcons(holder.ivTile, tileList.get(position - offset).getTileId());
             //holder.ivTile.setBackgroundResource(ivTiles[position]);
         }
     }
@@ -143,7 +145,7 @@ public class TileAdapter extends RecyclerView.Adapter<TileAdapter.TileViewHolder
         /*if (ActivityTileHome.finalSection || ActivityTileHome.showIndia) {
             return tileList.size() + 1;
         } else*/
-        return tileList.size();
+        return tileList.size() + 1;
     }
 
     public class TileViewHolder extends RecyclerView.ViewHolder {
